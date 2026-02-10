253b77972dab54c39e0c6a0d75981f6de325bdf8
Anas Shaikh <anasshaikh.ttw@gmail.com>
2026-02-10 19:11:36 +0530

    Add favicon and update website pages

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ if (form) {
         }
 
         try {
-            const res = await fetch('/api/send-message.js', {
+            const res = await fetch('/api/send-message', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(data)
